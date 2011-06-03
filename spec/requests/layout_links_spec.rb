f253e09545e2f12a8816cd540a6ef576e07a035b
Tamillharasan <tamillharasan.c@multunus.com>
2011-06-03 14:25:04 +0530

    Finished layouts and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,25 +2,42 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   describe "GET /layout_links" do
- it "should have a Home page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "Home")
-  end
+    it "should have a Home page at '/'" do
+      get '/'
+      response.should have_selector('title', :content => "Home")
+    end
 
-  it "should have a Contact page at '/contact'" do
-    get '/contact'
-    response.should have_selector('title', :content => "Contact")
-  end
+    it "should have a Contact page at '/contact'" do
+      get '/contact'
+      response.should have_selector('title', :content => "Contact")
+    end
 
-  it "should have an About page at '/about'" do
-    get '/about'
-    response.should have_selector('title', :content => "About")
-  end
-  
-  it "should have a Help page at '/help'" do
-    get '/help'
-    response.should have_selector('title', :content => "Help")
-  end
-end
+    it "should have an About page at '/about'" do
+      get '/about'
+      response.should have_selector('title', :content => "About")
+    end
+    
+    it "should have a Help page at '/help'" do
+      get '/help'
+      response.should have_selector('title', :content => "Help")
+    end
+    
+    it  "should have right links in layout" do
+      visit root_path
+      click_link "About"
+      response.should have_selector('title',:content => 'About')
 
+      click_link "Contact"
+      response.should have_selector('title',:content => 'Contact')
+
+      click_link "Help"
+      response.should have_selector('title',:content => 'Help')
+      
+      click_link "Home"
+      response.should have_selector('title',:content => 'Home')
+      
+      click_link "Sign up now!"
+      response.should have_selector('title',:content => 'Sign up')
+    end
+  end    
 end
